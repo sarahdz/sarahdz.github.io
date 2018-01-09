42197822bdb64ce65acf03aed97ca3cf44d2e48e
sarahdz <sarita2001@hotmail.co.uk>
2018-01-09 12:20:40 +0000

    add week 3

---
+++ week3.js
@@ -0,0 +1,20 @@
+alert("hello!");
+
+if(1+1===2) { 
+alert("Maths still work!");
+}
+
+if (1+1===3) {
+	alert("oh dear");
+}
+function checkPassword() { 
+alert("definitely checking password");
+
+var password = document.getElementById("password").value;
+alert(password);
+ if (password === "sarah2001") {
+console.log(password + " is correct!");
+    }
+}else{
+	console.log("notright");}
+}
