7b5d76a85f8103519ef438871b6a4b9531706891
sarahdz <sarita2001@hotmail.co.uk>
2018-01-09 12:20:34 +0000

    add week 3

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
-function multiply (x,y) {return 
-x * y ;
+function multiply (x,y) {
+	return x * y ;
 }
 
 
@@ -13,17 +13,19 @@ var input1= document.querySelector("#question1").value;
 var input2= document.querySelector("#question2").value;
 var result= multiply(input1,input2);
 
+console.log(input1); 
+console.log(input2); 
 console.log(result); 
 document.querySelector("#answer").innerHTML= result;
 }
 
-function updatename (newname) 
- if (newname === "SARAH")
+function updatename (newname) {
+ if (newname === "SARAH"){
  document.querySelector("myname").innerHTML= " hey, my name is Sarah too!";
  } else {
  	document.querySelector("#myname").innerHTML= "Hello," + newname; 
  }
- {
+} 
  	
  	var nameField= document.querySelector("#nameinput");
  	nameField.oninput=function() {  
@@ -34,3 +36,4 @@ function updatename (newname)
 var colourField= document.querySelector("#colour");
 colourField.oninput=function() {  
   document.querySelector("#colourchange").style.color= colourField.value;
+}
